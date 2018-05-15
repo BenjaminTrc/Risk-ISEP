a070ef731b722b5c0e0f2eebaca489e04f035126
DDeredec <dderedec92@gmail.com>
2018-05-15 13:52:19 +0200

    Classe Player

---
+++ Risk'ISEP/src/game/Player.java
@@ -0,0 +1,20 @@
+package game;
+
+public class Player {
+
+	private int nb_player;
+	
+	public Player() {
+		
+	}
+	
+	public int getNbPlayer() {
+		return this.nb_player;
+	}
+	
+	public void setNbPlayer() {
+		
+	}
+	
+	
+}
