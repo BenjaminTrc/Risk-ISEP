64c28a529a46e77731722aaf14c8a41c5dee08fc
DDeredec <dderedec92@gmail.com>
2018-05-15 14:18:53 +0200

    Ajout StdDraw

--- RISK/src/Player.java
+++ RISK/src/Player.java
@@ -1,4 +1,17 @@
 
 public class Player {
 
+	private int nb_player;
+	
+	public Player() {
+		
+	}
+	
+	public int getNbPlayer() {
+		return this.nb_player;
+	}
+	
+	public void setNbPlayer(int Nb) {
+		this.nb_player = Nb;
+	}
 }
