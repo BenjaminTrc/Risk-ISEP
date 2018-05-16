573238c51f1bb4ca1fabc1bf7574fbaebb0298c1
DDeredec <dderedec92@gmail.com>
2018-05-16 11:05:39 +0200

    ajout des classes régions et territoires

---
+++ RISK/src/Region.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Region {
+
+	// ***** Attributs *****
+	
+	private String region_name;
+	private ArrayList<Territory> liste_territoires = new ArrayList<>();
+	
+	// ***** Constructeurs *****
+	
+	
+	// ***** Methodes *****
+	
+	
+	// ***** Getters / Setters *****
+	
+	public String getRegionName() {
+		return this.region_name;
+	}
+	
+	public void setRegionName(String n) {
+		this.region_name = n;
+	}
+	
+	
+}
