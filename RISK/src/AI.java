1783bbd1d75b5907f033d423570a1f464cab91ba
BenjaminTrc <torchut.benjamin@gmail.com>
2018-06-04 23:05:31 +0200

    AI abstract class

---
+++ RISK/src/AI.java
@@ -0,0 +1,18 @@
+
+public abstract class AI {
+
+	
+	// ***** Methodes *****
+	
+	public void AIInitialization() {
+		
+	}
+	
+	public void AIReinforcement() {
+		
+	}
+	
+	public void AIAttack() {
+		
+	}
+}
